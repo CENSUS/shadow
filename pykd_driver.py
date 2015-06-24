d4b71da42360242c7b02f0939d4e15608ff79e22
Patroklos Argyroudis <argp@census-labs.com>
2015-06-24 17:34:51 +0300

    windbg/pykd frontend and backend

---
+++ pykd_driver.py
@@ -0,0 +1,231 @@
+# shadow - De Mysteriis Dom Firefox
+
+import os
+import sys
+import getopt
+import warnings
+
+sys.path.append('.')
+
+import shadow
+import pykd_engine as dbg
+
+true = True
+false = False
+none = None
+
+if __name__ == '__main__':
+    argc = len(sys.argv)
+
+    # don't remove this (still required?)
+    tmp = shadow.pykd.dbgCommand('?? mozglue!arenas[0]')
+
+    if argc == 1:
+        shadow.help()
+        sys.exit()
+
+    if sys.argv[1] == 'help':
+        shadow.help()
+
+    elif sys.argv[1] == 'jeparse':
+        shadow.parse()
+
+    elif sys.argv[1] == 'version':
+        shadow.version()
+
+    elif sys.argv[1] == 'jedump':
+        arg = ''
+
+        try:
+            arg = sys.argv[2]
+            screen = false
+        except:
+            screen = true
+
+        shadow.dump_all(filename = arg, dump_to_screen = screen)
+
+    elif sys.argv[1] == 'jechunks':
+        shadow.dump_chunks()
+
+    elif sys.argv[1] == 'jearenas':
+        shadow.dump_arenas()
+
+    elif sys.argv[1] == 'jeruns':
+        current_runs = false
+        size = 0
+        
+        try:
+    
+            alist, args = getopt.getopt(sys.argv[2:], 'cs:')
+
+            for (field, val) in alist:
+
+                if field in '-c':
+                    current_runs = true
+
+                if field in '-s':
+                    size = int(val)
+
+        except:
+            
+            print('[shadow] usage: jeruns [-cs]')
+            print('[shadow] options:')
+            print('[shadow]    -c           display current runs only')
+            print('[shadow]    -s <size>    runs of the given size class only')
+            print('[shadow] for example: jeruns -s 256 ')
+            sys.exit()
+
+        shadow.dump_runs(dump_current_runs = current_runs, size_class = size)
+
+    elif sys.argv[1] == 'jebins':
+        shadow.dump_bins()
+
+    elif sys.argv[1] == 'nursery':
+        shadow.dump_nursery()
+
+    elif sys.argv[1] == 'jeregions':
+        size_class = 0
+
+        try:
+            size_class = int(sys.argv[2])
+        except:
+            print('[shadow] usage: jeregions <size class>')
+            print('[shadow] for example: jeregions 1024')
+            sys.exit()
+
+        shadow.dump_regions(size_class)
+    
+    elif sys.argv[1] == 'pa':
+        addr = 0
+        new_len = 0x666
+
+        try:
+            if sys.argv[2].startswith('0x'):
+                addr = dbg.to_int(sys.argv[2])
+                new_len = dbg.to_int(sys.argv[3])
+            else:
+                addr = dbg.to_int('0x%s' % (sys.argv[2]))
+                new_len = dbg.to_int(sys.argv[3])
+        except:
+            if addr != 0:
+                shadow.pwnarray(addr)
+                sys.exit()
+            else:
+                print('[shadow] usage: pa <address> [<new length>]')
+                print('[shadow] for example: pa 0x13f1fc00 0x1000')
+                sys.exit()
+
+        shadow.pwnarray(addr, new_length = new_len)
+
+    elif sys.argv[1] == 'jeinfo':
+        addr = 0
+
+        try:
+            if sys.argv[2].startswith('0x'):
+                addr = dbg.to_int(sys.argv[2])
+            else:
+                addr = dbg.to_int('0x%s' % (sys.argv[2]))
+        except:
+            print('[shadow] usage: jeinfo <address>')
+            print('[shadow] for example: jeinfo 0x079e5440')
+            sys.exit()
+
+        shadow.dump_address(addr)
+
+    elif sys.argv[1] == 'jerun':
+        addr = 0
+
+        try:
+            if sys.argv[2].startswith('0x'):
+                addr = dbg.to_int(sys.argv[2])
+            else:
+                addr = dbg.to_int('0x%s' % (sys.argv[2]))
+        except:
+            print('[shadow] usage: jerun <address>')
+            print('[shadow] for example: jeinfo 0x087e1000')
+            sys.exit()
+
+        shadow.dump_run(addr)
+
+    elif sys.argv[1] == 'symbol':
+        size = 0
+        vtable = false
+        mozjs = false
+        xul = false
+        dom = false
+
+        try:
+
+            alist, args = getopt.getopt(sys.argv[2:], 'vjdx')
+
+            for (field, val) in alist:
+
+                if field in '-v':
+                    vtable = true
+
+                if field in '-j':
+                    mozjs = true
+
+                if field in '-d':
+                    dom = true
+
+                if field in '-x':
+                    xul = true
+
+            size = int(args[0])
+
+            if mozjs == dom == xul == false:
+                raise Exception()
+
+        except:
+
+            print('[shadow] usage: symbol [-vjdx] <size>')
+            print('[shadow] options:')
+            print('[shadow]    -v  only class symbols with vtable')
+            print('[shadow]    -j  only symbols from SpiderMonkey')
+            print('[shadow]    -d  only DOM symbols')
+            print('[shadow]    -x  only non-SpiderMonkey symbols')
+            sys.exit()
+
+        shadow.dump_symbol(size, has_vtable = vtable, from_mozjs = mozjs, \
+                from_xul = xul, from_dom = dom)
+
+    elif sys.argv[1] == 'jesearch':
+        search_for = ''
+        current_runs = false
+        quick = false
+        size_class = 0
+
+        try:
+
+            alist, args = getopt.getopt(sys.argv[2:], 'cs:q')
+
+            for (field, val) in alist:
+                
+                if field in '-c':
+                    current_runs = true
+
+                if field in '-s':
+                    size_class = int(val)
+                    
+                if field in '-q':
+                    quick = true
+
+            search_for = args[0]
+
+        except:
+
+            print('[shadow] usage: jesearch [-cqs] <hex dword>')
+            print('[shadow] options:')
+            print('[shadow]    -c           search current runs only')
+            print('[shadow]    -q           quick search')
+            print('[shadow]    -s <size>    regions of the given size only')
+            print('[shadow] for example: jesearch -c -s 256 0x41424344')
+            sys.exit()
+
+        shadow.search(search_for, region_size = size_class, \
+                search_current_runs = current_runs, quick_search = quick)
+
+    sys.exit()
+
+# EOF
---
+++ pykd_engine.py
@@ -0,0 +1,118 @@
+# shadow - De Mysteriis Dom Firefox
+
+import sys
+
+true = True
+false = False
+none = None
+
+sys.path.append('.')
+
+try:
+    import pykd
+except ImportError:
+    print('[shadow] pykd_engine is only usable from within windbg/pykd')
+    sys.exit()
+
+address_separator = '  '
+
+# windbg expressions for parsing arenas
+arena_expr = 'mozglue!arenas[%d]'
+arena_reg_size_expr = 'mozglue!arenas[%d]->bins[%d].reg_size'
+arena_reg0_offset_expr = 'mozglue!arenas[%d]->bins[%d].reg0_offset'
+arena_runcur_expr = 'mozglue!arenas[%d]->bins[%d].runcur'
+arena_bin_addr_expr = '&mozglue!arenas[%d]->bins[%d]'
+
+# windbg expressions for parsing all runs and their regions
+chunk_map_expr = '((mozglue!arena_chunk_t *)0x%x)->map'
+chunk_map_dump_expr = 'dp %x L%d'
+
+# windbg expressions for parsing current runs
+regs_mask_expr = 'dyb /c1 mozglue!arenas[%d]->bins[%d].runcur.regs_mask L%d'
+regs_mask_addr_expr = '((mozglue!arena_run_t *)0x%x)->regs_mask'
+regs_mask_addr_bits_expr = 'dyb /c1 0x%x L%d'
+
+# windbg expressions for parsing chunks
+chunk_rtree_root_expr = 'mozglue!chunk_rtree->root'
+chunk_rtree_height_expr = 'mozglue!chunk_rtree->height'
+chunk_rtree_level2bits_expr = 'mozglue!chunk_rtree->level2bits[%d]'
+chunk_radix_expr = 'dp %x L%d'
+chunk_arena_expr = '((mozglue!arena_chunk_t *)0x%x)->arena'
+
+# windbg expressions for parsing nursery data
+nursery_expr = 'xul!nsXPConnect::gSelf->mRuntime->mJSRuntime->gc.nursery'
+
+def to_int(val):
+    sval = str(val)
+    start = sval.find('0x')
+    
+    if start != -1:
+        end = sval.find('\n')
+        
+        if end == -1:
+            return int(sval[start:], 16)
+        else:
+            return int(sval[start:end], 16)
+        
+    elif sval.startswith('unsigned int'):
+        return int(sval[len('unsigned int'):])
+    else:
+        return int(sval)
+
+def buf_to_le(buf):
+    # this function is from seanhn's tcmalloc_gdb
+    tmp = 0
+
+    for i in range(0, len(buf)):
+        tmp |= (buf[i] << i * 8)
+
+    return tmp
+
+def get_page_size():
+    return pykd.pageSize()
+
+def offsetof(struct_name, member_name):
+    sval = pykd.dbgCommand('?? #FIELD_OFFSET(%s, %s)' \
+            % (struct_name, member_name))
+
+    if sval.startswith('long 0n'):
+        return int(sval[len('long 0n'):])
+    elif sval.startswith('long 0x'):
+        return int(sval[len('long 0x'):], 16)
+    else:
+        return int(sval)
+
+def sizeof(type_name):
+    return to_int(pykd.dbgCommand('?? sizeof(%s)' % (type_name)))
+
+def get_value(symbol):
+    mozglue = pykd.module('mozglue')
+    symbol_addr = mozglue.offset(symbol)
+    return pykd.ptrDWord(symbol_addr)
+
+def eval_expr(expr):
+    sval = pykd.dbgCommand('?? %s' % (expr))
+    return sval
+
+def execute(expr):
+    sval = pykd.dbgCommand(expr)
+    return sval
+
+def read_memory(addr, size, proc = none):
+    return buf_to_le(pykd.loadBytes(addr, size))
+
+def search(start_addr, end_addr, dword):
+    search_expr = 's -[1]d %x %x %s'
+    results = []
+
+    search_str = search_expr % (start_addr, end_addr, dword)
+    out_str = pykd.dbgCommand(search_str)
+    str_results = out_str.split('\n')
+
+    for str_result in str_results:
+        if str_result.startswith('0x'):
+            results.append((str_result, start_addr))
+
+    return results
+
+# EOF
